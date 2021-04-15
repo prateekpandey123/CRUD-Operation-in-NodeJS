956fbb36982394bf9cb730d4bb0ac2820b2f27ac
Prateek Pandey <prateekpandey736@gmail.com>
2021-04-15 11:40:10 +0530

    first

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+
+const connectDB = require('./server/database/connection');
+
+const app = express();
+
+dotenv.config( { path : 'config.env'})
+
+const PORT = 3000
+
+app.use(morgan('tiny'));
+connectDB();
+
+app.use(bodyparser.urlencoded({ extended : true}))
+
+app.set("view engine", "ejs")
+
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+app.use('/', require('./server/routes/router'))
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
